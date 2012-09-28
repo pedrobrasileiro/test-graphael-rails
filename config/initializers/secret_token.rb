6f31b81350aadfd117cc8cc0e022853f84a32b36
Pedro Brasileiro <pedrobrasileiro@gmail.com>
2012-09-27 21:13:04 -0300

    Empty Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestGraphaelRails::Application.config.secret_token = 'f3eb4d6facec8c26c98fef8510f2fd15eb787fc07ec9327910e8fcee604b0519d63dfa53ee0aa0ce21aa1f622d3e7b0ed8a37c9d45ffd2f2fa43cc823a1311bf'
